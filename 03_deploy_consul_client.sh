8f9b28c5244ca0f7819051f49b6cffa76e62ea3c
Carsten Duch <carsten@hashicorp.com>
2021-06-24 11:53:38 +0200

    bla

--- 03_deploy_consul_client.sh
+++ 03_deploy_consul_client.sh
@@ -7,7 +7,10 @@ docker run \
         "datacenter":"bielefeld",
         "connect": {"enabled":true}
         }' \
-consul agent -node=consul-client -client=0.0.0.0 -join="consul-srv-1"
+consul agent -node=consul-client -client=0.0.0.0 -retry-join="consul-srv-1" -retry-join="consul-srv-2" -retry-join="consul-srv-3"
+
+
+
 
 docker exec consul-client /bin/sh -c "echo '{\"service\": {  \"name\": \"counting\",  \"id\": \"counting-1\",  \"port\": 9001,  \"check\": {    \"http\": \"http:\/\/counting-service:9001\/health\",    \"method\": \"GET\",    \"interval\": \"1s\",    \"timeout\": \"1s\"  },  \"connect\": {    \"sidecar_service\": {}  }}}' > /consul/config/counting.json"
 docker exec consul-client /bin/sh -c "echo '{\"service\": {  \"name\": \"dashboard\",  \"port\": 9002,  \"connect\": {    \"sidecar_service\": {      \"proxy\": {      \"upstreams\": [{        \"destination_name\": \"counting\",        \"local_bind_port\": 5000      }]    }    }  },  \"check\": {    \"id\": \"dashboard-check\",    \"http\": \"http:\/\/dashboard-service:9002\/health\",    \"method\": \"GET\",    \"interval\": \"1s\",    \"timeout\": \"1s\"  }}}' > /consul/config/dashboard.json"
--- 05_deploy_dashboard_svc.sh
+++ 05_deploy_dashboard_svc.sh
@@ -2,7 +2,7 @@
 
 docker pull hashicorp/dashboard-service:0.0.4
 
-docker run --network dev-network -p 9002:9002 -d -e PORT=9002 -e COUNTING_SERVICE_URL='http://localhost:9001' --name=dashboard-service hashicorp/dashboard-service:0.0.4
+docker run --network dev-network -p 9002:9002 -d -e PORT=9002 -e COUNTING_SERVICE_URL='http://localhost:5000' --name=dashboard-service hashicorp/dashboard-service:0.0.4
 
 
 docker exec consul-client consul connect proxy -sidecar-for dashboard > dashboard-proxy.log &
