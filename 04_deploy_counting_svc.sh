4a7b40c1a996715e50a0f213b0d4fc9e1a827810
Carsten Duch <carsten@hashicorp.com>
2021-06-25 11:26:19 +0200

    deleted old files

--- 04_deploy_counting_svc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker pull hashicorp/counting-service:0.0.2
-
-docker run --network dev-network -p 9001:9001 -d -e PORT=9001 --name=counting-service hashicorp/counting-service:0.0.2
-
-docker exec consul-client consul connect proxy -sidecar-for counting-1 > counting-proxy.log &
--- 05_deploy_dashboard_svc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker pull hashicorp/dashboard-service:0.0.4
-
-docker run --network dev-network -p 9002:9002 -d -e PORT=9002 -e COUNTING_SERVICE_URL='http://localhost:5000' --name=dashboard-service hashicorp/dashboard-service:0.0.4
-
-
-docker exec consul-client consul connect proxy -sidecar-for dashboard > dashboard-proxy.log &
